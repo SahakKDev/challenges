6789b02fd23b9996110ff1792b6980af66797d61
Sahak Keshishyan <sahak.keshishyan.dev@gmail.com>
2024-03-15 11:19:06 +0400

    add animations with framer motion

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,3 +1,15 @@
+import { motion } from 'framer-motion';
+
 export default function Badge({ caption }) {
-  return <span className="badge">{caption}</span>;
+  return (
+    <motion.span
+      className="badge"
+      animate={{
+        scale: [1, 1.2, 1],
+        transition: 0.3,
+      }}
+    >
+      {caption}
+    </motion.span>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import NewChallenge from './NewChallenge.jsx';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -15,13 +17,22 @@ export default function Header() {
 
   return (
     <>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      <AnimatePresence>
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      </AnimatePresence>
 
       <header id="main-header">
-        <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <Link to="/" className="link">
+          <h1>Challenges</h1>
+        </Link>
+        <motion.button
+          whileHover={{ scale: 1.1, backgroundColor: '#062760' }}
+          transition={{ type: 'spring', stiffness: 500 }}
+          onClick={handleStartAddNewChallenge}
+          className="button"
+        >
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
